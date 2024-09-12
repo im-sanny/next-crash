f1a3559763e77321bcad94721119df2892cf2f5f
im-sanny <raselparvezsanny@gmail.com>
2024-09-12 23:30:20 +0600

    add: added not found page for not founded route

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import { notFound } from "next/navigation";
+import React from "react";
 
-const BlogPage = ({params}) => {
-    const {id} = params;
-  return (
-    <div className='mt-6'>The blog id is {id}</div>
-  )
-}
+const BlogPage = ({ params }) => {
+  const { id } = params;
+  if (id === "3") {
+    notFound();
+  }
+  return <div className="mt-6">The blog id is {id}</div>;
+};
 
 export default BlogPage;
