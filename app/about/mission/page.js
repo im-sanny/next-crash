72706d13ebb75226ed81209abe291609af73ce70
im-sanny <raselparvezsanny@gmail.com>
2024-10-18 23:47:25 +0600

    add: client component added

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,9 +1,16 @@
-import React from "react";
-
+"use client";
 const mission = () => {
   return (
     <main className="mt-10">
       <div>This is mission page</div>
+      <div className="mt-5">
+        <button
+          className="bg-green-500 rounded-sm px-4 py-1"
+          onClick={() => console.log("I have clicked here")}
+        >
+          Click here
+        </button>
+      </div>
     </main>
   );
 };
