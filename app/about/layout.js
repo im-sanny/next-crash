43bd8167d50af74ad32766d8ae8f816836607a28
im-sanny <raselparvezsanny@gmail.com>
2024-10-20 23:54:59 +0600

    add: added metadata in about us page

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import React from "react";
 
+export const metadata = {
+  title: "About Us",
+  description: "This is a about us blog page",
+};
+
 const AboutLayout = ({ children }) => {
   return (
     <div>
