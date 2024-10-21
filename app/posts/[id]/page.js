97703de76d034175bc0bf60d3d054fbbc0b89d93
im-sanny <raselparvezsanny@gmail.com>
2024-10-21 13:04:56 +0600

    add: added dynamic metadata in post page

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,8 +1,19 @@
 import getPost from "@/lib/getPost";
 
+export async function generateMetadata({ params }) {
+  const { id } = params;
+  const post = await getPost(id);
+
+  return {
+    title: post.title,
+    description: post.body,
+  };
+}
+
 export default async function PostPage({ params }) {
   const { id } = params;
   const post = await getPost(id);
+
   return (
     <div className="mt-6">
       <h2 className="text-blue-500 uppercase">{post.title}</h2>
